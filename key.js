8e8798b46b931cb9898ffea8a3c5dd90ea26c167
mojjoiv <daltonongeche@gmail.com>
2023-06-19 15:23:51 +0300

    movie search

---
+++ key.js
@@ -0,0 +1,7 @@
+//* First, you need to get your own OMDb API key to run this code properly.
+//* For that, you have to sign up for an account at http://www.omdbapi.com/apikey.aspx.
+//* You'll be sent a verification link containing your API key via email.
+//* Then, you'll need to type that key below.
+
+// Sample API KEY - "fc1fef96"
+key = "23d93ae3";
